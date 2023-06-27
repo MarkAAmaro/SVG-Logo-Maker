2b0975db7ff3edff938093b44aee46149f087cc6
Mark Anthony <manthonyamaro@yahoo.com>
2023-06-26 21:29:00 -0500

    placed prompt using an async function to gather data from user. The fuction retunrs the promise to await for the input which will be stored. Console logged data to verify input message is recieved to start the remaining inputs to create a logo

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+async function generateLogo() {
+    const userInput = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'text',
+          message: 'Enter up to three characters:',
+          validate: function (input) {
+            return input.length > 0 && input.length <= 3;
+          },
+        }
+    ])};
+
+    console.log('Generated logo.svg');
+    generateLogo();
