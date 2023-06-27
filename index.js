914341d0d71adc606f0682d9bfb57eb3b833617c
Mark Anthony <manthonyamaro@yahoo.com>
2023-06-27 18:42:11 -0500

    added three more prompts to create the logo and the function that executes the promise

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-async function generateLogo() {
-    const userInput = await inquirer.prompt([
-        {
-          type: 'input',
-          name: 'text',
-          message: 'Enter up to three characters:',
-          validate: function (input) {
-            return input.length > 0 && input.length <= 3;
-          },
-        }
-    ])};
 
-    console.log('Generated logo.svg');
+async function generateLogo() {
+  const userInput = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters:',
+        validate: function (input) {
+          return input.length > 0 && input.length <= 3;
+        },
+      },
+      {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter the text color (keyword or hexadecimal):',
+      },
+      {
+        type: 'list',
+        name: 'shape',
+        message: 'Select a shape:',
+        choices: ['circle', 'triangle', 'square'],
+      },
+      {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter the shape color (keyword or hexadecimal):',
+      },
+]);
+}
     generateLogo();
